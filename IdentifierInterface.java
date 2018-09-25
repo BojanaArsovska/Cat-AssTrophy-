223a71c5f7414a46f6fa6d61b32b2b7ab1b2cdd6
Harsh Khandelwal <97hknl@gmail.com>
2018-09-25 19:12:35 +0200

    Changed libraries and removed using namespace std

---
+++ IdentifierInterface.java
@@ -0,0 +1,16 @@
+
+public interface IdentifierInterface {
+	String data;
+	int length;
+	
+	/*
+	 * preconditions:
+	 * postconditoins: returns true if the identifer :begins with a letter, has length > 1 and has only alphanumeric characters
+	 */
+	public boolean isValidIdentifer();
+	
+	//fwddsf
+	
+	
+	//fewefdwdvw
+}
